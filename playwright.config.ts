fc3e5b2b5e7ec260c1779f1747eb75b5c3f497c3
Jan Beránek <86586714+jan-beranek@users.noreply.github.com>
2024-12-09 13:54:50 +0100

    Initial commit

---
+++ playwright.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig, devices } from "@playwright/test";
+
+export default defineConfig({
+  testDir: "./tests/",
+  snapshotPathTemplate: "baselines{/projectName}/{testFilePath}/{arg}{ext}",
+  fullyParallel: true,
+  timeout: 100000,
+  reporter: "html",
+  use: {
+    trace: "on-first-retry",
+  },
+
+  projects: [
+    {
+      name: "chrome",
+      use: {
+        headless: true,
+        viewport: { width: 1600, height: 1000 },
+        channel: "chrome",
+      },
+    },
+  ],
+});
