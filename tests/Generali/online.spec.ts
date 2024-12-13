f7669f2762092d327647d410860ec8a8371a4e41
Marcel Veselka <marcel.veselka@tesena.com>
2024-12-13 10:18:05 +0100

    feat: generali demo

---
+++ tests/Generali/online.spec.ts
@@ -0,0 +1,15 @@
+import { test } from "../../utils/fixtures";
+import { checkPage } from "../../utils/wopee";
+
+import websites from "./websites.json";
+
+test.describe("Generali Visual Testing - Demo", () => {
+  for (const website of websites) {
+    const scenarioName = `Page: ${website.name}`;
+    test(scenarioName, async ({ page, wopee }, testInfo) => {
+      await page.goto(website.url);
+      await page.click(website.cookieLocator);
+      await checkPage(page, wopee, scenarioName, testInfo.project.name);
+    });
+  }
+});
